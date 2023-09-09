f9447060a804e1f1ec6858d599243e33e6652d8e
LucaTaglialatela <45466948+LucaTaglialatela@users.noreply.github.com>
2023-09-09 14:00:41 +0800

    Add main file

---
+++ modern_cpp.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <string>
+#include <string.h>
+
+#include "sec4_challenge.h"
+#include "sec6_challenge.h"
+#include "sec7_challenge.h"
+#include "sec8_challenge.h"
+// #include "sec9_challenge.h"
+#include "sec10_challenge.h"
+#include "sec11_challenge.h" // Refactored sec9 challenge
+#include "sec12_challenge.h"
+#include "sec13_challenge.h"
+#include "sec14_challenge.h"
+// #include "sec15_challenge.h"
+#include "sec16_challenge.h" // Refactored sec15 challenge
+
+int main() {
+    // section_4_challenge();
+    // section_6_challenge();
+    // section_7_challenge();
+    // section_8_challenge();
+    // section_9_challenge();
+    // section_10_challenge();
+    // section_11_challenge();
+    // section_12_challenge();
+    // section_13_challenge();
+    // section_14_challenge();
+    // section_15_challenge();
+    section_16_challenge();
+
+    return 0;
+}
